ed06a29fa5c1008770f116d2219fb005d44f42d1
ankhod <ankhod7@gmail.com>
2025-03-11 09:13:15 +0200

    test-1

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import './App.css';
+
+// src/App.jsx
+
+function Product() {
+  return (
+    <div>
+            <h2>Cookies</h2>
+      <p>Price: 999 credits</p>   {' '}
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+            <h1>Products</h1>
+            <Product />
+            <Product />
+            <Product />   {' '}
+    </div>
+  );
+}
